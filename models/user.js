d5ac9433964a6b92aa989e3de4c78619616f7218
NguyenVuongw <nguyenvuongw134@gmail.com>
2026-04-23 09:34:08 +0700

    feat: add User model

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const { model, Schema } = require("mongoose");
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = model("User", userSchema, "users");
